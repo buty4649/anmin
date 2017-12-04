01d6fc6845f26f6ffe7a045d330398baaeceb569
Yuki Koya <ykky@pepabo.com>
2017-12-05 02:08:20 +0900

    定期実行できるようになった

--- main.go
+++ main.go
@@ -2,20 +2,58 @@ package main
 
 import (
     "fmt"
+    "os"
+    "os/signal"
+    "syscall"
+    "time"
+
     "github.com/buty4649/anmin/driver"
 )
 
 func main() {
+
     t, err := tsl256x.Open("/dev/i2c-1")
     if err != nil {
         panic(err)
     }
 
+    fin := make(chan bool)
+    sigCh := make(chan os.Signal, 1)
+    signal.Notify(sigCh, syscall.SIGINT, syscall.SIGHUP, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT)
+
+    go func() {
+        ticker := time.NewTicker(1 * time.Second)
+
+        if err:= printLux(t); err != nil {
+            panic(err)
+            fin <- true
+        }
+
+        for {
+            select {
+            case <-sigCh:
+                fin <- true
+                return
+
+            case <-ticker.C:
+                if err:= printLux(t); err != nil {
+                    panic(err)
+                    fin <- true
+                }
+            }
+        }
+    }()
+    <-fin
+
+    t.Close()
+    os.Exit(0)
+}
+
+func printLux(t *tsl256x.TSL256X) error{
     lux, err := t.ReadLux()
     if err != nil {
-        panic(err)
+        return err
     }
-
     fmt.Printf("%.1f\n", lux)
-    t.Close()
+    return nil
 }
